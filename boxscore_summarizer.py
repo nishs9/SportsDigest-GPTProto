bb814fc407c25583e22a1e6cf27d2f14d5dcb4fc
Nishant Sethunath <snishk@gmail.com>
2023-05-18 16:06:17 -0700

    Finished the boxscore summary script

--- boxscore_summarizer.py
+++ boxscore_summarizer.py
@@ -1,21 +1,39 @@
 import os
-import openai 
+import openai
 
-# read text from table.txt
-data = ""
-with open('table.txt', 'r') as file:
-    data = file.read()
+def generate_all_summaries():
+  # iterate through boxscores and return file name
+  for file in os.listdir('boxscores'):
+    if file.endswith('.txt'):
+      print(file)
+      generate_single_summary(file)
 
-prompt = "Give me a brief summary of a game between the Boston Red Sox and Atlanta Braves from the following box score. Avoid being overly verbose. Just provide a basic summary of the game, and some standout performers:\n\n"
+def generate_single_summary(file):
+  # read text from table.txt
+  data = ""
+  with open(f'boxscores/{file}', 'r') as file:
+      data = file.read()
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+  game_id = file.name.split('-')[0].split('/')[1]
+  away_team = file.name.split('-')[2]
+  home_team = file.name.split('-')[1]
 
-response = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
-        {"role": "system", "content": "You are a helpful assistant that summarizes MLB boxscores."},
-        {"role": "user", "content": prompt + data},
-    ]
-)
+  prompt = f"Give me a brief summary of a game between the {away_team} and {home_team} from the following box score. Avoid being overly verbose. Just provide a basic summary of the game, and some standout performers:\n\n"
 
-print(response.choices[0].message['content'])
+  openai.api_key = os.getenv("OPENAI_API_KEY")
+
+  response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+          {"role": "system", "content": "You are a helpful assistant that summarizes MLB boxscores."},
+          {"role": "user", "content": prompt + data},
+      ]
+  )
+
+  summary = response.choices[0].message['content']
+  summary_file_name = f'summaries/{game_id}-{away_team}-{home_team}-summary.txt'
+  with open(summary_file_name, 'w') as f:
+    f.write(summary)
+
+if __name__ == '__main__':
+  generate_all_summaries()
--- scraper.py
+++ scraper.py
@@ -22,28 +22,39 @@ def generate_team_mapping_csv():
         with open('team_mapping.csv', 'a') as csv:
             csv.write(displayName + ',' + abbreviation + '\n')
 
-def get_game_ids():
+# Retrieves the ESPN game id of all finished MLB games of the current day
+def get_completed_games():
     scoreboard_url = "http://site.api.espn.com/apis/site/v2/sports/baseball/mlb/scoreboard"
 
     response = requests.get(scoreboard_url)
     data = response.json()
 
     game_ids = []
+    away_teams = []
+    home_teams = []
 
     for event in data['events']:
-        game_ids.append(event['id'])
+        if (event['status']['type']['completed'] == True):
+            game_ids.append(event['id'])
+            home_teams.append(event['competitions'][0]['competitors'][0]['team']['displayName'])
+            away_teams.append(event['competitions'][0]['competitors'][1]['team']['displayName'])
 
-    return game_ids
+    if len(game_ids) == 0:
+        print('No games have finished today.')
+        return game_ids
+    
+    return (game_ids, home_teams, away_teams)
 
-def save_boxscore_tables(game_id):
+def save_boxscore_tables(game_id, home_team, away_team):
     url = f'https://www.espn.com/mlb/boxscore/_/gameId/{game_id}'
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    tables = soup.find_all('table')
+    file_name = f"boxscores/{game_id}-{away_team}-{home_team}-boxscore.txt"
+    with open(file_name, 'w') as f:
+        f.write(f'{away_team} vs {home_team} Box Score\n\n')
 
-    with open(test_file_name, 'w') as f:
-        f.write('Braves vs Red Sox Box Score\n\n')
+    tables = soup.find_all('table')
 
     for i, table in enumerate(tables):
         if i == 0:
@@ -57,15 +68,13 @@ def save_boxscore_tables(game_id):
         data = [[td.text for td in row.find_all('td')] for row in data_rows]
 
         df = pd.DataFrame(data, columns=columns)
-        print(df)
-        print()
 
         # write the df to a single text file as previous iterations and do not overwrite
-        with open(test_file_name, 'a') as f:
+        with open(file_name, 'a') as f:
             f.write(df.to_string())
             f.write('\n\n')
 
 if __name__ == "__main__":
-    save_boxscore_tables('401471565')
-    game_ids = get_game_ids()
-    print(game_ids)
+    games_info = get_completed_games()
+    for id, home_team, away_team in zip(games_info[0], games_info[1], games_info[2]):
+        save_boxscore_tables(id, home_team, away_team)
